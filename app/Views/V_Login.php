daa6d05e40543f072c3653919f8d85803b565811
muhammadhanifarafi <hanifarafi7@gmail.com>
2022-10-21 14:34:52 +0700

    Testing

--- app/Views/V_Login.php
+++ app/Views/V_Login.php
@@ -27,7 +27,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Halaman Login | Silog AJS</title>
+    <title>Halaman Login | Silog AJS Jakarta</title>
 
     <meta name="description" content="" />
 
